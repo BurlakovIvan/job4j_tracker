e6caf7fcc58b96ad55a6db484979a1d0039341ae
BurlakovIvan <burlakovid@inbox.ru>
2022-06-16 10:24:38 +0300

    2. Отсортировать департаменты [#34352 #312888]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -7,14 +7,9 @@ import java.util.List;
 public class DepDescComp implements Comparator<String> {
     @Override
     public int compare(String o1, String o2) {
-        List<String> o1Array = Arrays.asList(o1.split("/"));
-        List<String> o2Array = Arrays.asList(o2.split("/"));
-        int result = o2Array.get(0).compareTo(o1Array.get(0));
-        if (result == 0 && (o1Array.size() > 1 || o2Array.size() > 1)) {
-            String o1WihtoutFirst = o1.contains("/") ?  o1.substring(o1.indexOf("/") + 1) : "";
-            String o2WihtoutFirst = o2.contains("/") ?  o2.substring(o2.indexOf("/") + 1) : "";
-            result = o1WihtoutFirst.compareTo(o2WihtoutFirst);
-        }
-        return result;
+        String[] o1Array = o1.split("/");
+        String[] o2Array = o2.split("/");
+        int result = o2Array[0].compareTo(o1Array[0]);
+        return result != 0 ? result : o1.compareTo(o2);
     }
 }
