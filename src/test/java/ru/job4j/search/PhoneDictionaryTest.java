71c2ce036f13c0874b847fd59b4327d3db912e6e
BurlakovIvan <burlakovid@inbox.ru>
2022-06-16 09:01:05 +0300

    2.1. Лямбда блок [#249208]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -5,7 +5,8 @@ import org.junit.Test;
 import java.util.ArrayList;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public class PhoneDictionaryTest {
     @Test
