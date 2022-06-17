3ff18523f2e4b4eabb896f150ab2dc671068e5d8
BurlakovIvan <burlakovid@inbox.ru>
2022-06-18 00:45:29 +0300

    2. Список адресов [#110062 #313603]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -2,14 +2,15 @@ package ru.job4j.stream;
 
 import java.util.Comparator;
 import java.util.List;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
 
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
                 .map(Profile::getAddress)
-                .collect(Collectors.toList());
+                .collect(toList());
     }
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
@@ -17,6 +18,6 @@ public class Profiles {
                 .map(Profile::getAddress)
                 .sorted(Comparator.comparing(Address::getCity))
                 .distinct()
-                .collect(Collectors.toList());
+                .collect(toList());
     }
 }
