4d8550654b5e8ef18b9e54cc6fa4ad316b3cf184
BurlakovIvan <burlakovid@inbox.ru>
2022-06-19 21:47:13 +0300

    0. Stream API улучшения [#58167]

--- src/main/java/ru/job4j/stream/Student.java
+++ src/main/java/ru/job4j/stream/Student.java
@@ -12,6 +12,10 @@ public class Student {
         this.surname = surname;
     }
 
+    public Student(String surname, int score) {
+        this(score, surname);
+    }
+
     public int getScore() {
         return score;
     }
@@ -36,5 +40,13 @@ public class Student {
     @Override
     public int hashCode() {
         return Objects.hash(score, surname);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{"
+                + "score=" + score
+                + ", surname='" + surname + '\''
+                + '}';
     }
 }
