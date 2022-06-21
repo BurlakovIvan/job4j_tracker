40830b3469df71fcbd936fb76213d9023a6b16a9
BurlakovIvan <burlakovid@inbox.ru>
2022-06-21 13:15:28 +0300

    2. Аттестация [#49777 #314855]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -51,7 +51,6 @@ public class Analyze {
         return stream.flatMap(s -> s.getSubjects().stream())
                 .collect(Collectors
                         .groupingBy(Subject::getName,
-                                LinkedHashMap::new,
                                 Collectors.summingDouble(Subject::getScore)))
                 .entrySet()
                 .stream()
